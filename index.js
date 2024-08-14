b8c0edcbcb96391ad3da61b8338775d51439404b
Estercade <withrwithoutyou@gmail.com>
2024-08-13 23:11:33 -0400

    Add index.js server file and webpages
    Add index.js Node.js server file which listens for port 8080
    Add pages for index, about, contact me, and 404 pages
    Update README details regarding webpages

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    const url = req.url;
+    
+    if (url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), function(err, content) {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    } else if (url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), function(err, content) {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    } else if (url === '/contact-me') {
+        fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), function(err, content) {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    } else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), function(err, content) {
+            if (err) throw err;
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
+}).listen(8080);
